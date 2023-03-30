f8a3c7815f20ab23ff56352f0d0c9eff17a86684
Emmvid <emmavidarsson@hotmail.com>
2023-03-30 11:08:49 +0200

    cleaned up the code a bit

--- Database_Final_Project/Program.cs
+++ Database_Final_Project/Program.cs
@@ -1,6 +1,4 @@
 ﻿using Database_Final_Project.Services;
-
-
 // Skapar de olika statustyperna direkt när applikationen startas
 StatusService statusService = new();
 await statusService.CreateStatusIfNotExistsAsync();
